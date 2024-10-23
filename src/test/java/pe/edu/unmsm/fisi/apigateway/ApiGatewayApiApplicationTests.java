06044e254d9d84dad261c90404b82bb83e214f0b
Estiven Salvador Hurtado Santos <santosestiven46@gmail.com>
2024-10-23 08:14:48 -0500

    First commit

---
+++ src/test/java/pe/edu/unmsm/fisi/apigateway/ApiGatewayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.unmsm.fisi.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
